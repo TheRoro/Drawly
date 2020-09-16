eca8f5214d4a32df563f168b00554799cb1c9f41
TheRoro <39977138+TheRoro@users.noreply.github.com>
2020-09-16 10:49:12 -0500

    Removed package-lock

--- src/components/HomeComponent.js
+++ src/components/HomeComponent.js
@@ -1,12 +1,19 @@
 import React, {Component} from 'react';
+import { Jumbotron } from 'reactstrap';
 
 class Home extends Component {
     render() {
         return(
-            <div>
-                <h1>
-                    This is the home component
-                </h1>
+            <div className="home">
+                <Jumbotron className="row align-items-center">
+                    <div className="container">
+                        <div className="row justify-content-center align-items-center">
+                            <div className="col-auto">
+                                <h1>Useless App!</h1>
+                            </div>
+                        </div>
+                    </div>
+                </Jumbotron>
             </div>
         );
     }
