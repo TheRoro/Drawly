e7f0a4a8c0180c6203c620f214abec1995f2f614
TheRoro <39977138+TheRoro@users.noreply.github.com>
2022-01-31 08:08:21 -0500

    ⚡️Setup Project
    Added Tailwind and React Router dependencies. Removed default CRA files.

--- src/index.js
+++ src/index.js
@@ -1,17 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import './index.css'
+import App from './App'
+import JoinRoom from './components/joinRoom'
+import WaitingRoom from './components/waitingRoom'
+import AddTopic from './components/addTopic'
+import DrawTopic from './components/drawTopic'
+import ViewDraws from './components/viewDraws'
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+	<Router>
+		<Routes>
+			<Route path='/' element={<App />}></Route>
+			<Route path='/join' element={<JoinRoom />}></Route>
+			<Route path='/waiting' element={<WaitingRoom />}></Route>
+			<Route path='/topic' element={<AddTopic />}></Route>
+			<Route path='/draw' element={<DrawTopic />}></Route>
+			<Route path='/view' element={<ViewDraws />}></Route>
+			<Route path='/leaders'></Route>
+		</Routes>
+	</Router>,
+	document.getElementById('root')
+)
