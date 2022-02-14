7f2773f54dc4710da0390176cab206dd08cbe4aa
TheRoro <39977138+TheRoro@users.noreply.github.com>
2022-02-14 12:48:08 -0500

    ✨Add Home Page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,35 @@
-function App() {
-	return <section className='w-screen h-screen bg-neutral-800'></section>
+import { useNavigate } from 'react-router-dom'
+
+const App = () => {
+	let navigate = useNavigate()
+
+	return (
+		<section className='w-screen h-screen bg-paper-pattern bg-no-repeat bg-cover'>
+			<div className='w-full h-full p-12 flex flex-col lg:flex-row justify-center'>
+				<div className='w-full lg:w-2/3 h-1/3 lg:h-full flex flex-col justify-center items-center lg:items-center p-5 gap-14'>
+					<h1 className='font-pacifico text-9xl text-ink-200 text-center'>
+						Drawly
+					</h1>
+					<h2 className='font-shadows text-7xl text-ink-200 text-center'>
+						Fun With Friends
+					</h2>
+				</div>
+				<div className='w-full lg:w-1/3 h-1/3 lg:h-full flex flex-col items-center lg:items-start justify-center p-5 gap-6'>
+					<button
+						className='transition ease-in-out bg-blue-300 hover:bg-blue-400 w-72 py-3 px-4 font-medium text-xl rounded-lg shadow mt-12'
+						onClick={() => {
+							navigate('/join')
+						}}
+					>
+						Join Room
+					</button>
+					<button className='transition ease-in-out bg-emerald-300 hover:bg-emerald-400 w-72 py-3 px-4 font-medium text-xl rounded-lg shadow'>
+						Create Room
+					</button>
+				</div>
+			</div>
+		</section>
+	)
 }
 
 export default App
--- src/index.js
+++ src/index.js
@@ -3,22 +3,11 @@ import ReactDOM from 'react-dom'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import './index.css'
 import App from './App'
-import JoinRoom from './components/joinRoom'
-import WaitingRoom from './components/waitingRoom'
-import AddTopic from './components/addTopic'
-import DrawTopic from './components/drawTopic'
-import ViewDraws from './components/viewDraws'
 
 ReactDOM.render(
 	<Router>
 		<Routes>
 			<Route path='/' element={<App />}></Route>
-			<Route path='/join' element={<JoinRoom />}></Route>
-			<Route path='/waiting' element={<WaitingRoom />}></Route>
-			<Route path='/topic' element={<AddTopic />}></Route>
-			<Route path='/draw' element={<DrawTopic />}></Route>
-			<Route path='/view' element={<ViewDraws />}></Route>
-			<Route path='/leaders'></Route>
 		</Routes>
 	</Router>,
 	document.getElementById('root')
