e292d4f45e97df3366eac2d65ff340c14b0e7cc6
TheRoro <39977138+TheRoro@users.noreply.github.com>
2022-02-16 12:54:27 -0500

    📝 Update Tailwind config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,25 @@
 module.exports = {
 	content: ['./src/**/*.{js,jsx,ts,tsx}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				'paper-100': '#F9EDD1',
+				'paper-200': '#EEE2C7',
+				'paper-300': '#EADEC4',
+				'ink-100': '#6B6B6B',
+				'ink-200': '#383B3D',
+				golden: '#FFD165',
+				silver: '#E0E0E0',
+				bronze: '#E7C679',
+			},
+			backgroundImage: {
+				'paper-pattern': "url('/public/bg.svg')",
+			},
+			fontFamily: {
+				pacifico: ['Pacifico'],
+				shadows: ['"Shadows Into Light Two"'],
+			},
+		},
 	},
 	plugins: [],
 }
