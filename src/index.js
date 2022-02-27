3a916991994bd0ed96f0a8792f03dbb4bc0d1953
TheRoro <39977138+TheRoro@users.noreply.github.com>
2022-02-27 15:33:03 -0500

    ⚡️New Routes

--- src/index.js
+++ src/index.js
@@ -3,11 +3,23 @@ import ReactDOM from 'react-dom'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import './index.css'
 import App from './App'
+import JoinRoom from './pages/joinRoom'
+import WaitingRoom from './pages/waitingRoom'
+import AddTopic from './pages/addTopic'
+import DrawTopic from './pages/drawTopic'
+import ViewDraws from './pages/viewDraws'
+import LeaderBoard from './pages/leaderboard'
 
 ReactDOM.render(
 	<Router>
 		<Routes>
 			<Route path='/' element={<App />}></Route>
+			<Route path='/join' element={<JoinRoom />}></Route>
+			<Route path='/waiting' element={<WaitingRoom />}></Route>
+			<Route path='/topic' element={<AddTopic />}></Route>
+			<Route path='/draw' element={<DrawTopic />}></Route>
+			<Route path='/view' element={<ViewDraws />}></Route>
+			<Route path='/leaders' element={<LeaderBoard />}></Route>
 		</Routes>
 	</Router>,
 	document.getElementById('root')
